fa933256267855c46d3d15f0f0f712f663441286
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-07-22 02:29:13 -0400

    create user token

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,6 +38,7 @@ module.exports = function(app){
                 res.status(201).send({
                     id_token: createToken(person2)
                 });
+            });
         });
     });
 
